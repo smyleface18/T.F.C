af4dac476a97b2f16008452b87241979b8975f05
Caleb <ca30850@gmail.com>
2026-03-20 16:43:35 -0500

    first commit

---
+++ src/main/java/com/startup/TFC/TfcApplication.java
@@ -0,0 +1,13 @@
+package com.startup.TFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfcApplication.class, args);
+	}
+
+}
